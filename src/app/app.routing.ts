f234adda9014a8c3c5cfec96fca4c9d765dded8c
Lencho Burka <lencho.burka@asb.co.nz>
2019-06-02 17:47:39 +1200

    Fix asset paths

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,10 +4,10 @@ import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
   { path: 'projects', component: ProjectsComponent },
   { path: 'contact', component: ContactComponent },
-  { path: '**', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 
 export const AppRoutes = RouterModule.forChild(routes);
